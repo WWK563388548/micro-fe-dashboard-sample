22d82bd60bee9617e06c685adab184ceb51c06eb
Weikai Wang <wc563388548@gmail.com>
2023-03-21 20:45:40 +0800

    Add Login & Logout func(Communicating Auth changes and state)

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -16,7 +16,8 @@ export default () => {
           console.log(nextPathname);
           history.push(nextPathname);
         }
-      }
+      },
+      onSignIn,
     });
 
     history.listen(onParentNavigate)
